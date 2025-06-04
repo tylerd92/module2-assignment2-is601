ea4d415eb67d9fea24d06936d5050fbc3de00263
tylerd92 <twdeans@gmail.com>
2025-06-04 18:53:56 -0400

    fix minor bug discovered in pr

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -4,7 +4,7 @@ parameters:
 @float: b
 returns float
 """
-def addition(a: float: b: float) -> float:
+def addition(a: float, b: float) -> float:
     """
     Calculates the sum of numbers a and b and returns the result
     """
