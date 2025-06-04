8d420378884ad51e534acf947e868ede7a414ff7
tylerd92 <twdeans@gmail.com>
2025-06-04 18:00:13 -0400

    create addition function

---
+++ app/operations/__init__.py
@@ -0,0 +1,12 @@
+"""
+parameters:
+@float: a
+@float: b
+returns float
+"""
+def addition(a: float: b: float) -> float:
+    """
+    Calculates the sum of numbers a and b and returns the result
+    """
+    return a + b
+
